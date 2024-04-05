0e8e587c9afa73827f4d1989fe837af3591d581d
ktjayamanna <kaveen.jayamanna@gmail.com>
2024-04-05 14:04:32 +0000

    now we can upload a json to s3

---
+++ bash_scripts/start_celery_worker.sh
@@ -0,0 +1,1 @@
+celery -A flask_async_api.celery worker --loglevel=info
--- flask_async_api.py
+++ flask_async_api.py
@@ -2,9 +2,15 @@ from flask import Flask, request, jsonify
 from celery import Celery
 from dotenv import load_dotenv
 import os
+import boto3
+import json
+from botocore.exceptions import NoCredentialsError
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.devcontainer', '.env')
 load_dotenv(dotenv_path)
+
+aws_access_key_id = os.getenv('MIN_PYRO_USER_AWS_ACCESS_KEY')
+aws_secret_access_key = os.getenv('MIN_PYRO_USER_AWS_SECRET_KEY')
 
 broker_url = os.getenv("BROKER_URL")
 app = Flask(__name__)
@@ -20,10 +26,28 @@ app.config['CELERY_RESULT_SERIALIZER'] = 'json'
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+# Initialize a Boto3 S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key)
+
 # Define a Celery task
-@celery.task
-def add(x, y):
-    return x + y
+@celery.task(bind=True)
+def add(self, x, y):
+    result = x + y
+    result_data = json.dumps({'result': result})
+    
+    # Generate an object key for the S3 bucket
+    object_key = f"tests/{self.request.id}.json"
+    bucket_name = 'workingdir--storage'
+
+    try:
+        # Upload the result data to S3
+        s3.put_object(Bucket=bucket_name, Key=object_key, Body=result_data)
+    except NoCredentialsError:
+        return "AWS credentials not found."
+
+    # Return the S3 object URL or key for client access
+    return f"s3://{bucket_name}/{object_key}"
 
 # Define a route to submit tasks
 @app.route('/add', methods=['POST'])
